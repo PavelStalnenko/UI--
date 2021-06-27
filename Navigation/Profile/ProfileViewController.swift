cdbaf77f491d30122983eb13e4a41b99312ae57c
PavelStalnenko <64463497+PavelStalnenko@users.noreply.github.com>
2021-06-27 13:12:30 +0300

    Готовое ДЗ

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    
+    let profileHeader = ProfileHeaderView()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
+        self.view.addSubview(profileHeader)
+
+        
     }
-    let profileHeader = ProfileHeaderView()
+    
     override func viewWillLayoutSubviews(){
         profileHeader.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-//        view.backgroundColor = .green 
+        
+        
+//        view.backgroundColor = .green
     }
+   
+    
     
     /*
      // MARK: - Navigation
